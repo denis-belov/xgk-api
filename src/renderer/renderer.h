b97ce951eeebbe11a61d7e1b364da44b9d1660f4
dsbelov63 <dsbelov63@gmail.com>
2021-12-11 19:49:30 +0400

    up

---
+++ src/renderer/renderer.h
@@ -0,0 +1,24 @@
+#ifndef __XGK_API_RENDERER__
+#define __XGK_API_RENDERER__
+
+
+
+#include <cstdint>
+
+
+
+namespace XGK
+{
+	namespace API
+	{
+		struct Renderer
+		{
+			std::size_t width {};
+			std::size_t height {};
+		};
+	}
+}
+
+
+
+#endif
