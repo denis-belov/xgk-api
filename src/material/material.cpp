1c5fcd1ea2ee70a7d6ec551f2994d9b0664f8f41
dsbelov63 <dsbelov63@gmail.com>
2022-01-08 13:55:04 +0400

    up

--- src/material/material.cpp
+++ src/material/material.cpp
@@ -9,16 +9,19 @@ namespace XGK
 		struct MaterialOffsets
 		{
 			size_t topology = offsetof(Material, topology);
+			size_t front_face = offsetof(Material, front_face);
 			size_t glsl100es_code_vertex = offsetof(Material, glsl100es_code_vertex);
 			size_t glsl100es_code_fragment = offsetof(Material, glsl100es_code_fragment);
 			size_t glsl300es_code_vertex = offsetof(Material, glsl300es_code_vertex);
 			size_t glsl300es_code_fragment = offsetof(Material, glsl300es_code_fragment);
-			size_t spirv_code_vertex = offsetof(Material, spirv_code_vertex);
-			size_t spirv_code_fragment = offsetof(Material, spirv_code_fragment);
+			size_t glsl4_code_vertex = offsetof(Material, glsl4_code_vertex);
+			size_t glsl4_code_fragment = offsetof(Material, glsl4_code_fragment);
 			size_t glsl_vulkan_code_vertex = offsetof(Material, glsl_vulkan_code_vertex);
 			size_t glsl_vulkan_code_fragment = offsetof(Material, glsl_vulkan_code_fragment);
 			size_t wgsl_code_vertex = offsetof(Material, wgsl_code_vertex);
 			size_t wgsl_code_fragment = offsetof(Material, wgsl_code_fragment);
+			size_t spirv_code_vertex = offsetof(Material, spirv_code_vertex);
+			size_t spirv_code_fragment = offsetof(Material, spirv_code_fragment);
 			size_t uniforms = offsetof(Material, uniforms);
 			size_t uniform_blocks = offsetof(Material, uniform_blocks);
 			size_t descriptor_sets = offsetof(Material, descriptor_sets);
